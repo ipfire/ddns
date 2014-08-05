89eab5205e24a0ef3d47da79934b904249ae3d03
Naugrimm <Naugrimm@users.noreply.github.com>
2014-08-05 14:31:41 +0200

    spdns.org: IPv6 support
    see http://wiki.securepoint.de/index.php/SPDNS_FAQ#Unterst.C3.BCtzt_der_Server_die_Verwendung_von_IPv6-Adressen.3F

--- src/ddns/providers.py
+++ src/ddns/providers.py
@@ -955,7 +955,7 @@ class DDNSProviderSPDNS(DDNSProtocolDynDNS2, DDNSProvider):
 	handle    = "spdns.org"
 	name      = "SPDNS"
 	website   = "http://spdns.org/"
-	protocols = ("ipv4",)
+	protocols = ("ipv4", "ipv6")
 
 	# Detailed information about request and response codes are provided
 	# by the vendor. They are using almost the same mechanism and status
@@ -965,6 +965,18 @@ class DDNSProviderSPDNS(DDNSProtocolDynDNS2, DDNSProvider):
 	# http://wiki.securepoint.de/index.php/SPDNS_Update-Tokens
 
 	url = "https://update.spdns.de/nic/update"
+	
+	@property
+	def proto(self):
+		return self.get("proto")
+
+	def _prepare_request_data(self):
+		data = {
+			"hostname" : self.hostname,
+			"myip"     : self.get_address(self.proto)
+		}
+
+		return data
 
 
 class DDNSProviderStrato(DDNSProtocolDynDNS2, DDNSProvider):
