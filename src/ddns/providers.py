99dcedbf2dd705f57c2c084f479ca4ba52f88d6f
dajoe <johannes.schwietzke@web.de>
2020-12-29 12:07:19 +0100

    Update providers.py
    Python 3 memoryview requires an encoded string.

--- src/ddns/providers.py
+++ src/ddns/providers.py
@@ -536,7 +536,7 @@ class DDNSProviderBindNsupdate(DDNSProvider):
 
 			logger.debug("  %s" % line)
 
-		return "\n".join(scriptlet)
+		return "\n".join(scriptlet).encode()
 
 
 class DDNSProviderChangeIP(DDNSProvider):
